57a8198c99d5395f9d8113de156a2b925d40c3db
AdithyaKrishnaMenon <127472657+AdithyaKrishnaMenon@users.noreply.github.com>
2023-04-17 10:24:30 +0530

    Update mqttpub.py

--- mqttpub.py
+++ mqttpub.py
@@ -4,6 +4,6 @@ import paho.mqtt.client as mqtt
 payload="Hello"
 topic="IOT/test"
 client = mqtt.Client()
-client.connect('35.244.58.37',1883,60)
+client.connect('34.93.71.238',1883,60)
 (rc,mid)=client.publish(topic,payload);
 client.disconnect();
